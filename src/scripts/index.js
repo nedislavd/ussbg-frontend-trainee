9b7e247acba7d8dc305737bae899877e6bd84e60
vbc4255 <nedislav.denev@uss.bg>
2019-03-22 14:27:50 +0200

    Added Task 3 Requirements

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 import '../styles/index.scss';
 import './tasks/task2';
+import './tasks/task3';
 
 console.log('Hello from the console sideeeeee...');
---
+++ src/scripts/tasks/task3.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(function () {
+    console.log('||Task 3 Bellow||');
+
+
+})();
