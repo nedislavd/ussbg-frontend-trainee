b24e49a2f1156a371fe1f5742e46e6f357010bc2
elena-penkova <elenap02@abv.bg>
2019-05-02 16:45:03 +0300

    tasks 2&9

--- src/scripts/tasks/task2.js
+++ src/scripts/tasks/task2.js
@@ -1,68 +1,21 @@
 
 
-
-function solve([n]) {
-    let result = [1];
-    let displayResult = [];
-    for (let i = 0; i < n; i++) {
-        for (let j = 1; j < n; j++) {
-            if ()
-           result[i][j] = result[i][j-1] + result[i][j];
-           console.log(result[i][j])
-        }
-    }
-
- }
-solve(["5"]);
-
-function solve2([n]) {
-    n = Number(n);
-    let result = [];
-    result[0] = [1];
-    result[1] = [1,1];
-    for (let row = 1; row < n; row++) {
-        console.log(" ".repeat(n - row));
-        for (let col = row - 1 ; col <= row; col++) {
-            if (row == col || col == 0) {
-                result[row][col] = 1;
-            }
-            // if ()
-            // else result = result * (row - col + 1) / col;
-            debugger;
-            result[row][col] = result[row-1][col-1] + result[row-1][col];
-
-            console.log(result + ' ');
-        }
-    }
-
-    return(result)
-}
-
-solve2(["5"]);
-
-function solve3([n]) {
-    n = Number(n);
-    let result = 1;
-    let displayResult = [];
-    displayResult[0] = [1];
-    displayResult[1] = [1,1];
-    for (let row = 0; row < n; row++) {
-        console.log(" ".repeat(n - row));
-        for (let col = 0; col <= row; col++) {
-            if (row == 0 || col == 0) {
-                result = 1
-                displayResult[row].push(1);
+let generatePascalsTriangle = (numberOfRows) => {
+    numberOfRows = Number(numberOfRows);
+    let triangle = [];
+    for (let row = 0; row < numberOfRows; row++) {
+        triangle[row] = [row + 1];
+        for (let col = 0; col < row + 1; col++) {
+            if (col == 0 || col == row) {
+                triangle[row][col] = 1;
             } else {
-                result = result * (row - col + 1) / col;
-                displayResult[row][col] = result * (row - col + 1) / col;
+                triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col]
             }
-
-            console.log(result + ' ');
         }
     }
-
-    console.log(displayResult)
-}
-
-solve3(["5"]);
-
+    console.log(triangle);
+    for (let displayRow = 0; displayRow < triangle.length; displayRow++) {
+        console.log(triangle[displayRow] + '\n')
+    }
+};
+generatePascalsTriangle(5);
--- src/scripts/tasks/task9.js
+++ src/scripts/tasks/task9.js
@@ -1,11 +1,37 @@
-'use strict';
+let generatePascalsTriangle = (numberOfRows) => {
+    numberOfRows = Number(numberOfRows);
+    let triangle = [];
 
-(function () {
-    console.log('||Task 9 Bellow||');
+    for (let row = 0; row < numberOfRows; row++) {
+        triangle[row] = [row + 1];
+        for (let col = 0; col < row + 1; col++) {
+            if (col == 0 || col == row) {
+                triangle[row][col] = 1;
+            } else {
+                triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col]
+            }
+        }
+    }
+    console.log(triangle);
 
-    /* Tips:
-    *   - Read up on document content loaded
-    *   - Read up on document.createElement & document.appendChild
-    *   - Read up on how to attach events (click) to elements
-    * */
-})();
+    for (let displayRow = 0; displayRow < triangle.length; displayRow++) {
+        var para = document.createElement("P");
+        document.getElementById("myDIV").appendChild(para);
+
+        for (let i = 0; i < triangle[displayRow].length; i++) {
+            var rowEl = document.createElement("SPAN");
+            rowEl.innerHTML = triangle[displayRow][i];
+            para.appendChild(rowEl);
+        }
+    }
+
+};
+
+function myFunction() {
+    var div = document.getElementById('myDIV');
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+    generatePascalsTriangle(document.getElementById("myInput").value);
+}
+
